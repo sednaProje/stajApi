cf70b4e124f12ce65b48e88f50e20ab4f90f2d16
Ali Küçükkaya <115551844+sednaProje@users.noreply.github.com>
2022-10-18 10:47:57 +0300

    a

--- Proje.API/Proje.API/Startup.cs
+++ Proje.API/Proje.API/Startup.cs
@@ -30,7 +30,7 @@ namespace Proje.API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Proje.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Proje.API", Version = "v1.0" });
             });
         }
 
